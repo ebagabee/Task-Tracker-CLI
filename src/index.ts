eb43e6ceba608f1f578622cbd93de9fc5624c350
ebagabee <2001.gabrielhas@gmail.com>
2024-10-26 10:08:28 -0300

    ADD Method is resolve

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import readline from "readline";
 import fs from "fs/promises";
 import path from "path";
-import { randomUUID, UUID } from "crypto";
+import { randomUUID } from "crypto";
 
 const fileDatabase = path.join(__dirname, "database/database.json");
 
@@ -17,7 +17,7 @@ function askQuestion(query: string): Promise<string> {
 interface Task {
   id: string;
   description: string;
-  status: string;
+  status: statusQuestionEnum;
   createdAt: Date;
   updatedAt: Date;
 }
@@ -29,25 +29,17 @@ enum questionEnumInput {
   MARK_PROGRESS = "mark-in-progress",
   MARK_DONE = "mark-done",
   LIST = "list",
-  DONE = "done",
-  TODO = "todo",
-  IN_PROGRESS = "in-progress",
 }
 
-let choice: string;
+enum statusQuestionEnum {
+  TODO = "todo",
+  IN_PROGRESS = "in-progress",
+  DONE = "done",
+}
 
-async function add() {
+async function add(): Promise<void> {
   try {
     const description = await askQuestion("Description: ");
-    const status = await askQuestion("Status (todo, in-progress, done): ");
-
-    if (!["todo", "in-progress", "done"].includes(status)) {
-      console.log(
-        "Invalid status! Please use 'todo', 'in-progress', or 'done'."
-      );
-      rl.close();
-      return;
-    }
 
     const data = await fs.readFile(fileDatabase, "utf-8");
     const tasks: Task[] = JSON.parse(data);
@@ -55,7 +47,7 @@ async function add() {
     const newTask: Task = {
       id: randomUUID(),
       description,
-      status,
+      status: statusQuestionEnum.TODO,
       createdAt: new Date(),
       updatedAt: new Date(),
     };
@@ -72,39 +64,46 @@ async function add() {
   }
 }
 
-rl.question("What do you want to do?: ", (current) => {
-  choice = current;
+async function main(){
 
-  switch (choice) {
-    case questionEnumInput.ADD:
-      add();
-      break;
-    case questionEnumInput.UPDATE:
-      console.log("Updating...");
-      break;
-    case questionEnumInput.DELETE:
-      console.log("Deleting...");
-      break;
-    case questionEnumInput.MARK_PROGRESS:
-      console.log("Mark progress");
-      break;
-    case questionEnumInput.MARK_DONE:
-      console.log("Making done...");
-      break;
-    case questionEnumInput.LIST:
-      console.log("Listing...");
-      break;
-    case questionEnumInput.DONE:
-      console.log("Doning");
-      break;
-    case questionEnumInput.TODO:
-      console.log("Todo task");
-      break;
-    case questionEnumInput.IN_PROGRESS:
-      console.log("In progress task");
-      break;
-    default:
-      console.log("That's impossible");
-      break;
+  try {
+    const choice = await askQuestion("What is the current task?");
+
+    switch (choice) {
+      case questionEnumInput.ADD:
+        await add();
+        break;
+      case questionEnumInput.UPDATE:
+        console.log("Updating...");
+        break;
+      case questionEnumInput.DELETE:
+        console.log("Deleting...");
+        break;
+      case questionEnumInput.MARK_PROGRESS:
+        console.log("Mark progress");
+        break;
+      case questionEnumInput.MARK_DONE:
+        console.log("Making done...");
+        break;
+      case questionEnumInput.LIST:
+        console.log("Listing...");
+        break;
+      case statusQuestionEnum.DONE:
+        console.log("Done");
+        break;
+      case statusQuestionEnum.TODO:
+        console.log("Todo task");
+        break;
+      case statusQuestionEnum.IN_PROGRESS:
+        console.log("In progress task");
+        break;
+      default:
+        console.log("That's impossible");
+        break;
+    }
+  } catch (error) {
+    console.error("Error:", error);
   }
-});
+}
+
+main();
